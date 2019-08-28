389396f29066b3c2e7e8509033faa54a2f74f1ac
Demon000 <demonsingur@gmail.com>
2019-08-28 22:40:11 +0200

    Revert "power: qcom: import xiaomi changes"
    This reverts commit a002bd3eac71ae98a9d9dee8a6909c61d86db2ec.

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -985,26 +985,8 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 
 	vote(chip->pl_disable_votable, ICL_CHANGE_VOTER, false, 0);
 
-	if (!chip->usb_psy)
-		chip->usb_psy = power_supply_get_by_name("usb");
-	if (!chip->usb_psy) {
-		pr_err("Couldn't get usb psy\n");
-		return -ENODEV;
-	}
-
-	rc = power_supply_get_property(chip->usb_psy,
-				POWER_SUPPLY_PROP_SMB_EN_REASON, &pval);
-	if (rc < 0) {
-		pr_err("Couldn't get cp reason rc=%d\n", rc);
-		return rc;
-	}
-
-	if (chip->cp_ilim_votable) {
-		if (pval.intval != POWER_SUPPLY_CP_WIRELESS)
-			vote(chip->cp_ilim_votable, ICL_CHANGE_VOTER, true, icl_ua);
-		else
-			vote(chip->cp_ilim_votable, ICL_CHANGE_VOTER, false, 0);
-	}
+	if (chip->cp_ilim_votable)
+		vote(chip->cp_ilim_votable, ICL_CHANGE_VOTER, true, icl_ua);
 
 	return 0;
 }
